7532d943f420215c102e5d88eecafcc1816e479b
jovibfel@gmail.com <jovibfel@gmail.com>
2025-09-08 22:18:47 -0300

    Update UrlMappingRepository.java

--- src/main/java/com/jfelixy/encurtadorurl/repository/UrlMappingRepository.java
+++ src/main/java/com/jfelixy/encurtadorurl/repository/UrlMappingRepository.java
@@ -2,9 +2,11 @@ package com.jfelixy.encurtadorurl.repository;
 
 import com.jfelixy.encurtadorurl.model.UrlMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
 
     Optional<UrlMapping> findByShortKey(String shortKey);
